4891bace1a6fc8cf8f9e31df5f1270edbcf1adcb
Jason Constam <callmebob@gmail.com>
2019-08-16 22:00:42 +0200

    Simple implementation of date

--- src/commands/date.cpp
+++ src/commands/date.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
+#include <ctime>
 
 using namespace std;
 
 int run_date( int argc, char* argv[ ] )
 {
+    time_t currTime = time( 0 );
+    struct tm* timeData = localtime( &( currTime ) );
+    char buffer[ 100 ] = { 0 };
+
+    strftime( buffer, 100, "%A %B %d %Y, %H:%M:%S (UTC%z)", timeData );
+
+    cout << buffer << endl;
+
     return 0;
 }
