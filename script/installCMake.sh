103b1747ab79d18f5c12ae709c8ec934e38f4bc0
Jason Constam <callmebob@gmail.com>
2019-08-23 00:49:07 +0200

    Added script for installing CMake

---
+++ script/installCMake.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt-get remove --purge --auto-remove cmake
+mkdir temp
+cd temp/
+wget https://github.com/Kitware/CMake/releases/download/v3.15.2/cmake-3.15.2.tar.gz
+tar xzvf cmake-3.15.2.tar.gz
+cd cmake-3.15.2/
+./bootstrap
+sudo make -j install
+rm -rf temp
