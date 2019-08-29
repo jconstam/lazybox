65f07f4364cb72c3f2dd5a04282c1eee2d1fb424
Jason Constam <callmebob@gmail.com>
2019-08-28 23:35:10 -0600

    Basic implementation of cat.

--- src/commands/cat.cpp
+++ src/commands/cat.cpp
@@ -5,7 +5,11 @@
     @function run_cat
  */
 
+#include <string>
+#include <fstream>
 #include <iostream>
+
+#include <sys/stat.h>
 
 #include "parseArgs.h"
 
@@ -23,6 +27,20 @@ static const ARG_DATA argInfo[ PARAM_COUNT ] =
     { 'n', false, ARG_DATA_TYPE_BOOL, offsetof( CAT_PARAMS, numberOutputLines ) }
 };
 
+static string readEntireFile( string filePath )
+{
+    ifstream fileStream( filePath );
+    string fileContents;
+
+    fileStream.seekg( 0, ios::end );   
+    fileContents.reserve( fileStream.tellg( ) );
+    fileStream.seekg( 0, ios::beg );
+
+    fileContents.assign( istreambuf_iterator<char>( fileStream ), istreambuf_iterator<char>( ) );
+
+    return fileContents;
+}
+
 int run_cat( int argc, char* argv[ ] )
 {
     int startIndex;
@@ -31,6 +49,36 @@ int run_cat( int argc, char* argv[ ] )
     if( !parseArgs( argInfo, PARAM_COUNT, &( params ), &( startIndex ), argc, argv ) )
     {
         return 1;
+    }
+
+    int fileIndex;
+    for( fileIndex = startIndex; fileIndex < argc; fileIndex++ )
+    {
+        string filePath = argv[ fileIndex ];
+
+        struct stat buffer;   
+        if( stat( filePath.c_str( ), &( buffer ) ) != 0 )
+        {
+            cout << "cat: " << filePath << ": No such file or directory" << endl;
+            return 1;
+        }
+
+        int lineNumber = 1;
+        int stringIndex = 0;
+        string fileContents = readEntireFile( filePath );
+        while( ( size_t ) stringIndex < fileContents.length( ) )
+        {
+            int nextStringIndex = fileContents.find( "\n", stringIndex );
+
+            if( params.numberOutputLines )
+            {
+                cout.width( 6 );
+                cout << right << lineNumber++ << "  ";
+            }
+            cout << fileContents.substr( stringIndex, nextStringIndex - stringIndex ) << endl;
+
+            stringIndex = nextStringIndex + 1;
+        }
     }
 
     return 0;
