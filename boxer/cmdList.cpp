786ccb7ba69d44a2f0192dd001b309a621272a81
Jason Constam <callmebob@gmail.com>
2019-09-20 22:21:21 +0200

    Isolated boxer's build and run to be its own make target

---
+++ boxer/cmdList.cpp
@@ -0,0 +1,12 @@
+// This file was automatically generated.  Do not modify.
+
+#include "cmdList.hpp"
+
+const std::map<std::string, CmdFunc> CmdList::commandList =
+{
+	{ "basename", run_basename },
+	{ "cat", run_cat },
+	{ "cksum", run_cksum },
+	{ "date", run_date },
+	{ "echo", run_echo },
+};
