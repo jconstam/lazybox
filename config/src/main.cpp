6f29a5d158b781129f1dbeac9f73d1a9a9577dab
Jason Constam <callmebob@gmail.com>
2019-09-28 23:12:33 +0200

    Moved some code to a common folder used by all projects.  Added start of a config generator.

---
+++ config/src/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main( int argc, char* argv[] )
+{
+	system( "clear" );
+
+	cout << "CONFIGURING" << endl;
+	cin.ignore( );
+
+	system( "clear" );
+
+	return 0;
+}
