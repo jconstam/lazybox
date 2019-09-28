eb055d5c33523158bb8b42b65a430261b8687815
Jason Constam <callmebob@gmail.com>
2019-09-28 21:58:58 +0200

    Rearranged includes for main

---
+++ src/include/commands.hpp
@@ -0,0 +1,12 @@
+// This file was automatically generated.  Do not modify.
+
+#ifndef COMMANDS_HPP__
+#define COMMANDS_HPP__
+
+extern "C" int run_basename( int argc, char* argv[ ] );
+int run_cat( int argc, char* argv[ ] );
+extern "C" int run_cksum( int argc, char* argv[ ] );
+int run_date( int argc, char* argv[ ] );
+extern "C" int run_echo( int argc, char* argv[ ] );
+
+#endif
