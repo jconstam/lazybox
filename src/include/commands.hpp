ca9cbf142ea5fce5588c337adbb5d6d373e6fbac
Jason Constam <callmebob@gmail.com>
2019-10-10 20:19:32 +0200

    Added static analysis with cppcheck

--- src/include/commands.hpp
+++ src/include/commands.hpp
@@ -3,10 +3,6 @@
 #ifndef COMMANDS_HPP__
 #define COMMANDS_HPP__
 
-extern "C" int run_basename( int argc, char* argv[ ] );
-int run_cat( int argc, char* argv[ ] );
 extern "C" int run_cksum( int argc, char* argv[ ] );
-int run_date( int argc, char* argv[ ] );
-extern "C" int run_echo( int argc, char* argv[ ] );
 
 #endif
